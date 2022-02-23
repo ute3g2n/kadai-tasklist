ab07c8b497bf2d3c865298520bc3b387dc7bd6d4
suguru.oue <ute3g2n@gmail.com>
2022-02-24 06:54:52 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 import javax.persistence.EntityManager;
 
 import java.sql.Timestamp;
@@ -21,28 +22,16 @@ public class NewServlet extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EntityManager em = DBUtil.createEntityManager();
-		em.getTransaction().begin();
-
-		// Taskのインスタンスを生成
-		Task t = new Task();
-
-		// tの各フィールドにデータを代入
-		String content = "hello";
-		t.setContent(content);
-
-		Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-		t.setCreated_at(currentTime);
-		t.setUpdated_at(currentTime);
-
-		// データベースに保存
-		em.persist(t);
-		em.getTransaction().commit();
-
-		// 自動採番されたIDの値を表示
-		response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-		em.close();
+		
+		// CSRF対策
+		request.setAttribute("_token", request.getSession().getId());
+		
+		// おまじないとしてのインスタンスを生成
+		request.setAttribute("task", new Task());
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
